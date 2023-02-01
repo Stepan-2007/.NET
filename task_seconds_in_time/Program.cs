f7129ebcd39fb6bef4ae66ba2d422fbe0840fdc7
Stepan-2007 <123959855+Stepan-2007@users.noreply.github.com>
2023-02-01 14:56:08 +0200

    Delete task_seconds_in_time directory

--- task_seconds_in_time/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿Console.WriteLine("Enter value seconds");
-int valueSeconds = int.Parse(Console.ReadLine());
-int seconds = valueSeconds % 60;
-int minutes = (valueSeconds / 60)%60;
-int hours = (valueSeconds / 60)/60;
-int days = ((valueSeconds / 60)/60)/60;
-Console.WriteLine($"days: {days} {hours}:{minutes}:{seconds}");
