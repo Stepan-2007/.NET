525676cbf4f5232385377779aef0c9c0b429d99b
Stepan-2007 <123959855+Stepan-2007@users.noreply.github.com>
2023-02-01 14:56:18 +0200

    Delete task_year directory

--- task_year/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿Console.WriteLine("Enter year: ");
-int year= int.Parse(Console.ReadLine());
-if ((year % 4) == 0|| (year % 400) == 0)
-{
-    Console.WriteLine("366");
-}
-else Console.WriteLine("365");
