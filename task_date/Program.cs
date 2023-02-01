84bed3ec02bff2ce4f39cc8bcb8d2104fe5b8291
Stepan-2007 <123959855+Stepan-2007@users.noreply.github.com>
2023-02-01 14:55:45 +0200

    Delete task_date directory

--- task_date/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿Console.WriteLine("Enter year: ");
-int year = int.Parse(Console.ReadLine());
-Console.WriteLine("Enter month: ");
-int month = int.Parse(Console.ReadLine());
-Console.WriteLine("Enter day: ");
-int day = int.Parse(Console.ReadLine());
-Console.WriteLine($"{day}/{month}/{year}");
