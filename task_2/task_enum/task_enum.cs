e6100859700561fc619c20cddabb90fdb972002d
Stepan-2007 <manua.stepan.2007@gmail.com>
2023-02-08 17:13:42 +0200

    task123

---
+++ task_2/task_enum/task_enum.cs
@@ -0,0 +1,37 @@
+﻿
+enum Week { Monday=1, Tuesday , Wednesday , Thursday, Friday, Saturday, Sunday }
+internal class task_enum
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Enter number day: ");
+        Week day = Enum.Parse<Week>(Console.ReadLine());
+        switch (day)
+        {
+            case Week.Monday:
+                Console.WriteLine($"{Week.Monday}");
+                break;
+            case Week.Tuesday:
+                Console.WriteLine($"{Week.Tuesday}");
+                break;
+            case Week.Wednesday:
+                Console.WriteLine($"{Week.Wednesday}");
+                break;
+            case Week.Thursday:
+                Console.WriteLine($"{Week.Thursday}");
+                break;
+            case Week.Friday:
+                Console.WriteLine($"{Week.Friday}");
+                break;
+            case Week.Saturday:
+                Console.WriteLine($"{Week.Saturday}");
+                break;
+            case Week.Sunday:
+                Console.WriteLine($"{Week.Sunday}");
+                break;
+            default:
+                Console.WriteLine($"Invalid num day:{day}");
+                break;
+        }
+    }
+}
---
+++ task_2/task_exchange_currency/task_exchange.cs
@@ -0,0 +1,33 @@
+﻿enum Ccurency { USD = 1, EUR, PLN }
+internal class task_exchange
+{
+    private static void Main(string[] args)
+    {
+        const float USD = 37.28F;
+        const float EUR = 40.35F;
+        const float PLN = 8.53F;
+        Console.Write("Enter sum money: ");
+        int sumMoney =int.Parse( Console.ReadLine());
+        Console.WriteLine("Choose ccurency:\n" +
+                            $"{(int)Ccurency.USD} - {Ccurency.USD}\n" +
+                            $"{(int)Ccurency.EUR} - {Ccurency.EUR}\n" +
+                            $"{(int)Ccurency.PLN} - {Ccurency.PLN}\n");
+
+        Ccurency exchange = Enum.Parse<Ccurency>(Console.ReadLine());
+        switch (exchange)
+        {
+            case Ccurency.USD:
+                Console.WriteLine($"Result: {sumMoney*USD}");
+                break;
+            case Ccurency.EUR:
+                Console.WriteLine($"Result: {sumMoney * EUR}");
+                break;
+            case Ccurency.PLN:
+                Console.WriteLine($"Result: {sumMoney * PLN}");
+                break;
+            default:
+                Console.WriteLine("Invalid ccurency");
+                break;
+        }
+    }
+}
